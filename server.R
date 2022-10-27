2e482ecd5986226b8b1c22e8910475509d1f9eaa
Shivani Srivastava <shivani141296@gmail.com>
2022-10-27 09:43:36 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,17 +1,14 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+########################################################################
+#               A/B Testing - Digital Marketing                        #
+#               Back-end by Prof. Sudhir Voleti                        #
+#                 Shiny by Shivani Srivastava                          #
+########################################################################
 
 library(DT)
 library(shiny)
 require(tidyverse)
 
-# Define server logic required to draw a histogram
+
 shinyServer(function(input, output) {
 
   mydata <- reactive({
@@ -109,7 +106,7 @@ shinyServer(function(input, output) {
       conv_rate_baseline0 = baseline0_1/baseline0_2; conv_rate_baseline0
       
       # build empty DF to populate: outp_df
-      metric = c('Conv rate','Estimated Difference', 'Relative Uplift(%)', 'pooled sample proportion',  'Standard Error of Difference', 'z_score', 'p-value', 'Margin of Error',  'CI-lower', 'CI-upper')
+      metric = c('Conversion rate','Estimated Difference', 'Relative Uplift(%)', 'Pooled Sample Proportion',  'Standard Error of Difference', 'Z - score', 'p-value', 'Margin of Error',  'CI-lower', 'CI-upper')
       
       outcomes = unique(outcome0); outcomes
       outp_df = data.frame(matrix(0, nrow=length(metric), ncol=length(unique(unlist(variant0)))))
@@ -170,93 +167,30 @@ shinyServer(function(input, output) {
       outp_df } # func ends
     
     
-    conti_outcome_an <- function(mydata, variant0, baseline0=NA, outcome0){
-      
-      variants = unique(variant0)
-      #variants = levels(as.factor(variant0)); variants
-      if (is.na(baseline0)){baseline0 = levels(as.factor(variant0))[1]}; baseline0
-      
-      filt_ind0 = (variant0 == baseline0); filt_ind0[1:5]
-      avg_rate_baseline0 = outcome0[filt_ind0] %>% mean(.); avg_rate_baseline0
-      
-      # build empty DF to populate: outp_df
-      metric = c('Outcome_mean','Estimated Difference', 'Relative Uplift(%)', 'pooled sample mean',  'Standard Error of Difference', 'z_score', 'p-value', 'Margin of Error',  'CI-lower', 'CI-upper')
-      
-      # outcomes = levels(outcome0)
-      outp_df = matrix(NA, nrow=length(metric), ncol=length(variants))
-      rownames(outp_df) = metric
-      colnames(outp_df) = variants
-      
-      mean_pool = mean(outcome0); mean_pool 
-      outp_df[4,] = mean_pool %>% round(., 4)
-      
-      sd_pool = sd(outcome0); sd_pool
-      MOE_pool = sd_pool * qnorm(0.975); MOE_pool
-      outp_df[8,] = MOE_pool %>% round(., 4)
-      
-      # populate the DF now for each variant as colm
-      for (a0 in variants){ 
-        
-        a1 = which(variants == a0); a1
-        filt_ind0 = (variant0 == a0); filt_ind0[1:5]
-        avg_rate_a0 = outcome0[filt_ind0] %>% mean(.); avg_rate_a0
-        outp_df[1, a1] = avg_rate_a0 %>% round(., 4)
-        
-        se_a0 = outcome0[filt_ind0] %>% sd(.)
-        outp_df[5, a1] = se_a0 %>% round(., 4)
-        
-        est_diff_a0 = (avg_rate_a0 - avg_rate_baseline0)
-        outp_df[2,a1] = est_diff_a0 %>% round(., 4)
-        
-        uplift_a0 = (avg_rate_a0 - avg_rate_baseline0)*100/avg_rate_baseline0
-        outp_df[3,a1] = uplift_a0 %>% round(., 4)
-        
-        zscore_a0 = est_diff_a0/sd_pool
-        outp_df[6,a1] = zscore_a0 %>% round(., 4)
-        
-        pval_a0 = pnorm(q = -abs(zscore_a0), mean=0, sd=1)*2
-        outp_df[7,a1] = pval_a0 %>% round(., 4)
-        
-        ci_low_a0 = avg_rate_a0 - qnorm(0.975) * se_a0
-        ci_hi_a0 = avg_rate_a0 + qnorm(0.975) * se_a0
-        
-        outp_df[9,a1] = ci_low_a0 %>% round(., 4)
-        outp_df[10,a1] = ci_hi_a0 %>% round(., 4)
-      } # for loop ends
-      
-      outp_df } # func ends
     
     
-    output$checker <- renderPrint({
-      
-      baseline0_1 = mydata() |> filter(variant0() == baseline() & ioutcome0() == ioutpositive()) |> nrow()
-      baseline0_2 = mydata() %>% filter(variant0() == baseline()) %>% nrow()
-      cat(baseline0_1, baseline0_2)
-      cat(baseline0_1/baseline0_2)
-      
-      conv_rate_a0 = baseline0_1/baseline0_2
-      
-      se_a0 = sqrt(conv_rate_a0 * (1 - conv_rate_a0)/baseline0_2)
-      
-      cat(se_a0)
-      
-      p_pool = sum(ioutcome0() == rep(ioutpositive(), length(ioutcome0())))/length(ioutcome0())
-      cat(p_pool)
-    })
-    
-    output$dataframe <- renderPrint({
+    output$dataframe <- DT::renderDataTable({
       req(input$file)
       
-      outputdf = reactive({discrete_outcome_an(mydata(), variant0(),
+      outputdf = reactive({
+        discrete_outcome_an(mydata(), variant0(),
                                               baseline0 = baseline(),
                                               outcome0 = ioutcome0(),
-                                              outcome0_pos = ioutpositive())})
+                                              outcome0_pos = ioutpositive())
+        
+        
+        })
       
       as.data.frame(outputdf())
       
       })
     
     
-    
+    output$downloadData1 <- downloadHandler(
+      filename = function() {"abc_testdata.csv"},
+      content = function(file) {
+        write.csv("data/abCtestdta.csv", file)
+      }
+    )
     
 })
